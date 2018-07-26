440a66f6feca6caee43a1d767cffe096eda353e1
rickytricky <ricky10056@gmail.com>
2018-07-26 16:44:20 -0500

    Se agrego el componente de Usuarios
    :+1:

---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  mostrarInfo(){
+    alert('Producto Comprardo');
+  }
+
+}
