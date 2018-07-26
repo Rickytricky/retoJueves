76edd00315adeb8bc2eb3ca04bebf88b0da3f930
rickytricky <ricky10056@gmail.com>
2018-07-26 16:57:34 -0500

    Se acepto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
